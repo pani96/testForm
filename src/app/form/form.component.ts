23c3b3cc4c9c857f9ea171444bfcb86da019e17e
pani96 <pa-nida-1234@hotmail.com>
2019-02-17 15:33:07 +0700

    formanddetail-inputoutput

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup , Validator, Validators, FormControl, AbstractControl} from '@angular/forms';
 import { User } from '../user';
+
 
 @Component({
   selector: 'app-form',
@@ -10,6 +11,7 @@ import { User } from '../user';
 export class FormComponent implements OnInit {
  
   formGroup: FormGroup;
+  @Output() change = new EventEmitter();
   constructor(
     private fb: FormBuilder
   ) { 
@@ -18,11 +20,24 @@ export class FormComponent implements OnInit {
 
   ngOnInit() {
     this.formGroup = this.fb.group({
-      firstName: this.fb.control(''),
-      lastName: [''],
-      email: [''],
-      age: [22]
+      firstName: this.fb.control('',[Validators.required, Validators.minLength(2)]),
+      lastName: ['',[Validators.required, Validators.minLength(2)]],
+      email: ['',[this.emailValidator]],
+      age: [22,[Validators.min(0),Validators.max(99)]]
     })
+  }
+  emailValidator(control: AbstractControl){
+    const value: string = control.value;
+    if(value && value.includes('@')){
+      return null;
+    }
+    return {
+      email: true
+      /*email: {
+        acturl: value,
+        expect: 'email@example'
+      }*/
+    }
   }
   onSubmit(form: FormGroup){
     console.log(form);
@@ -30,8 +45,38 @@ export class FormComponent implements OnInit {
     console.log(value);*/
     /*const{firstName, lastName} = form.value;
     console.log(firstName,lastName);*/
-    const {firstName,lastName,email,age} = form.value;
-    const user = new User(firstName,lastName,email,age);
-    console.log(user);
+   
+    /*if(!(firstName && firstName.length >= 3)){
+      alert("hello");
+    }*/
+    console.log(form.valid,form.invalid);
+    console.log((<FormControl>form.get('firstName')).errors);
+    if(form.valid)
+    {const {
+      firstName,
+      lastName,
+      email,
+      age} = form.value;
+    const user = new User(
+      firstName,
+      lastName,
+      email,
+      age);
+      this.change.emit(user);
+    console.log(user);}
+    else{
+      ['firstName',
+      'lastName',
+      'email',
+      'age'
+      ].forEach((key: string)=>{
+        console.log(key);
+        console.log(form.get(key).touched);
+        form.get(key).markAsTouched();
+        console.log(form.get(key).touched);
+      })
+
+    }
+    
   }
 }
