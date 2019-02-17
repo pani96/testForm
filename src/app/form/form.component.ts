901e7bba7fb696646483684edffc94e65154bee2
pani96 <pa-nida-1234@hotmail.com>
2019-02-17 12:18:25 +0700

    updateform

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { User } from '../user';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+ 
+  formGroup: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) { 
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.formGroup = this.fb.group({
+      firstName: this.fb.control(''),
+      lastName: [''],
+      email: [''],
+      age: [22]
+    })
+  }
+  onSubmit(form: FormGroup){
+    console.log(form);
+    /*const value = form.value;
+    console.log(value);*/
+    /*const{firstName, lastName} = form.value;
+    console.log(firstName,lastName);*/
+    const {firstName,lastName,email,age} = form.value;
+    const user = new User(firstName,lastName,email,age);
+    console.log(user);
+  }
 }
